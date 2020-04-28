677e2910c032743e50cb5d0250b6c079013cc28a
greatyun <yjs8237@gmail.com>
2020-04-28 12:47:10 +0900

    테스트 코드 및 이것저것..

---
+++ src/main/java/com/greatyun/soccer/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.greatyun.soccer.member.repository;
+
+import com.greatyun.soccer.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member , Long> {
+
+}
